6bad494821e77ce2dde38311da0b199420a9cd1f
Frank3549 <fbautista@middlebury.edu>
2024-11-05 20:12:13 -0800

    fourth case of learn_decision_tree function

---
+++ decision.py
@@ -0,0 +1,347 @@
+"""
+CS311 Programming Assignment 5: Decision Trees
+
+Full Name: Frank Bautista
+
+Description of performance on Hepatitis data set:
+
+TODO: Answer the questions included in the assignment
+
+Description of Adult dataset discretization and selected features:
+
+TODO: Answer the questions included in the assignment
+
+Potential effects of using your model for marketing a cash-back credit card:
+
+TODO: Answer the questions included in the assignment
+"""
+
+import argparse, os, random, sys
+from typing import Any, Dict, Sequence, Tuple, Union
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+from sklearn.model_selection import StratifiedKFold
+
+# Type alias for nodes in decision tree
+DecisionNode = Union["DecisionBranch", "DecisionLeaf"]
+
+
+class DecisionBranch:
+    """Branching node in decision tree"""
+
+    def __init__(self, attr: str, branches: Dict[Any, DecisionNode]):
+        """Create branching node in decision tree
+
+        Args:
+            attr (str): Splitting attribute
+            branches (Dict[Any, DecisionNode]): Children nodes for each possible value of `attr`
+        """
+        self.attr = attr
+        self.branches = branches
+
+    def predict(self, x: pd.Series):
+        """
+        Return predicted labeled for array-like example x
+        
+        Args:
+            x (pd.Series): array-like example
+        
+        """
+        # TODO: Implement prediction based on value of self.attr in x
+
+    def display(self, indent=0):
+        """Pretty print tree starting at optional indent"""
+        print("Test Feature", self.attr)
+        for val, subtree in self.branches.items():
+            print(" " * 4 * indent, self.attr, "=", val, "->", end=" ")
+            subtree.display(indent + 1)
+
+
+
+class DecisionLeaf:
+    """Leaf node in decision tree"""
+
+    def __init__(self, label):
+        """Create leaf node in decision tree
+
+        Args:
+            label: Label for this node
+        """
+        self.label = label
+
+    def predict(self, x):
+        """Return predicted labeled for array-like example x"""
+        return self.label
+
+    def display(self, indent=0):
+        """Pretty print tree starting at optional indent"""
+        print("Label=", self.label)
+
+
+
+
+def information_gain(X: pd.DataFrame, y: pd.Series, attribute: str) -> float:
+    """
+        Return the expected reduction in entropy from splitting X,y by attr
+    Args:
+        X (pd.DataFrame): Table of examples (as DataFrame)
+        y (pd.Series): array-like example labels (target values)
+        attrs (Sequence[str]): Possible attributes to split examples
+    """
+
+    entropy = B(sum(y) / len(y))
+    attribute_values = X[attribute].unique() 
+    remainder = 0.0
+    for value in attribute_values:
+
+        attribute_value_indices = X[X[attribute] == value].index
+        y_labels = y[attribute_value_indices]
+        positive_instances = sum(y_labels)
+        total_instances = len(y_labels)
+
+        attribute_value_entropy = B(positive_instances / total_instances)
+        remainder += (len(y_labels) / total_instances) * attribute_value_entropy
+
+
+    return entropy - remainder
+
+def B(p: float) -> float:
+    """Return entropy"""
+
+    if p == 0 or p == 1:
+        return 0.0
+    
+    return -(p * np.log2(p) + (1 - p) * np.log2(1 - p))
+    
+
+
+
+def learn_decision_tree(
+    X: pd.DataFrame,
+    y: pd.Series,
+    attributes: Sequence[str],
+    y_parent: pd.Series,
+) -> DecisionNode:
+    """Recursively learn the decision tree
+
+    Args:
+        X (pd.DataFrame): Table of examples (as DataFrame)
+        y (pd.Series): array-like example labels (target values)
+        attrs (Sequence[str]): Possible attributes to split examples
+        y_parent (pd.Series): array-like example labels for parents (parent target values)
+
+
+
+    Returns:
+        DecisionNode: Learned decision tree node
+    """
+    # TODO: Implement recursive tree construction based on pseudo code in class
+    # and the assignment
+    
+    # Case 1: examples are empty
+    if X.empty:
+        return plurality_value(y_parent)
+
+    # Case 2: all examples have the same label
+    elif len(y.unique()) == 1:
+        return DecisionLeaf(y.iloc[0])
+
+    # Case 3: attributes is empty
+    elif len(attributes) == 0:
+        return plurality_value(y)
+
+    # Case 4: recursive tree construction
+    best_attribute = max(attributes, key=lambda attribute: information_gain(X, y, attribute))
+
+
+
+def plurality_value(y: pd.Series) -> DecisionLeaf:
+    """
+    Return a leaf node containing the most common value in y
+    
+    """
+    positive_instances = sum(y)
+    negative_instances = len(y) - positive_instances
+    if positive_instances >= negative_instances:
+        return DecisionLeaf(1)
+    else:
+        return DecisionLeaf(0)
+
+    
+
+
+def fit(X: pd.DataFrame, y: pd.Series) -> DecisionNode:
+    """Return train decision tree on examples, X, with labels, y"""
+    # You can change the implementation of this function, but do not modify the signature
+    return learn_decision_tree(X, y, X.columns, y)
+
+
+def predict(tree: DecisionNode, X: pd.DataFrame):
+    """Return array-like predictions for examples, X and Decision Tree, tree"""
+
+    # You can change the implementation of this function, but do not modify the signature
+
+    # Invoke prediction method on every row in dataframe. `lambda` creates an anonymous function
+    # with the specified arguments (in this case a row). The axis argument specifies that the function
+    # should be applied to all rows.
+    return X.apply(lambda row: tree.predict(row), axis=1)
+
+
+def load_adult(feature_file: str, label_file: str):
+
+    # Load the feature file
+    examples = pd.read_table(
+        feature_file,
+        dtype={
+            "age": int,
+            "workclass": "category",
+            "education": "category",
+            "marital-status": "category",
+            "occupation": "category",
+            "relationship": "category",
+            "race": "category",
+            "sex": "category",
+            "capital-gain": int,
+            "capital-loss": int,
+            "hours-per-week": int,
+            "native-country": "category",
+        },
+    )
+    labels = pd.read_table(label_file).squeeze().rename("label")
+
+
+    # TODO: Select columns and choose a discretization for any continuous columns. Our decision tree algorithm
+    # only supports discretized features and so any continuous columns (those not already "category") will need
+    # to be discretized.
+
+    # For example the following discretizes "hours-per-week" into "part-time" [0,40) hours and
+    # "full-time" 40+ hours. Then returns a data table with just "education" and "hours-per-week" features.
+
+    examples["hours-per-week"] = pd.cut(
+        examples["hours-per-week"],
+        bins=[0, 40, sys.maxsize],
+        right=False,
+        labels=["part-time", "full-time"],
+    )
+
+    return examples[["education", "hours-per-week"]], labels
+
+
+# You should not need to modify anything below here
+
+
+def load_examples(
+    feature_file: str, label_file: str, **kwargs
+) -> Tuple[pd.DataFrame, pd.Series]:
+    """Load example features and labels. Additional arguments are passed to
+    the pandas.read_table function.
+
+    Args:
+        feature_file (str): Delimited file of categorical features
+        label_file (str): Single column binary labels. Column name will be renamed to "label".
+
+    Returns:
+        Tuple[pd.DataFrame,pd.Series]: Tuple of features and labels
+    """
+    return (
+        pd.read_table(feature_file, dtype="category", **kwargs),
+        pd.read_table(label_file, **kwargs).squeeze().rename("label"),
+    )
+
+
+def compute_metrics(y_true, y_pred):
+    """Compute metrics to evaluate binary classification accuracy
+
+    Args:
+        y_true: Array-like ground truth (correct) target values.
+        y_pred: Array-like estimated targets as returned by a classifier.
+
+    Returns:
+        dict: Dictionary of metrics in including confusion matrix, accuracy, recall, precision and F1
+    """
+    return {
+        "confusion": metrics.confusion_matrix(y_true, y_pred),
+        "accuracy": metrics.accuracy_score(y_true, y_pred),
+        "recall": metrics.recall_score(y_true, y_pred),
+        "precision": metrics.precision_score(y_true, y_pred),
+        "f1": metrics.f1_score(y_true, y_pred),
+    }
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Train and test decision tree learner")
+    parser.add_argument(
+        "-p",
+        "--prefix",
+        default="small1",
+        help="Prefix for dataset files. Expects <prefix>.[train|test]_[data|label].txt files (except for adult). Allowed values: small1, tennis, hepatitis, adult.",
+    )
+    parser.add_argument(
+        "-k",
+        "--k_splits",
+        default=10,
+        type=int,
+        help="Number of splits for stratified k-fold testing",
+    )
+
+    args = parser.parse_args()
+
+    if args.prefix != "adult":
+        # Derive input files names for test sets
+        train_data_file = os.path.join(
+            os.path.dirname(__file__), "data", f"{args.prefix}.train_data.txt"
+        )
+        train_labels_file = os.path.join(
+            os.path.dirname(__file__), "data", f"{args.prefix}.train_label.txt"
+        )
+        test_data_file = os.path.join(
+            os.path.dirname(__file__), "data", f"{args.prefix}.test_data.txt"
+        )
+        test_labels_file = os.path.join(
+            os.path.dirname(__file__), "data", f"{args.prefix}.test_label.txt"
+        )
+
+        # Load training data and learn decision tree
+        train_data, train_labels = load_examples(train_data_file, train_labels_file)
+        tree = fit(train_data, train_labels)
+        tree.display()
+
+        # Load test data and predict labels with previously learned tree
+        test_data, test_labels = load_examples(test_data_file, test_labels_file)
+        pred_labels = predict(tree, test_data)
+
+        # Compute and print accuracy metrics
+        predict_metrics = compute_metrics(test_labels, pred_labels)
+        for met, val in predict_metrics.items():
+            print(
+                met.capitalize(),
+                ": ",
+                ("\n" if isinstance(val, np.ndarray) else ""),
+                val,
+                sep="",
+            )
+    else:
+        # We use a slightly different procedure with "adult". Instead of using a fixed split, we split
+        # the data k-ways (preserving the ratio of output classes) and test each split with a Decision
+        # Tree trained on the other k-1 splits.
+        data_file = os.path.join(os.path.dirname(__file__), "data", "adult.data.txt")
+        labels_file = os.path.join(os.path.dirname(__file__), "data", "adult.label.txt")
+        data, labels = load_adult(data_file, labels_file)
+
+        scores = []
+
+        kfold = StratifiedKFold(n_splits=args.k_splits)
+        for train_index, test_index in kfold.split(data, labels):
+            X_train, X_test = data.iloc[train_index], data.iloc[test_index]
+            y_train, y_test = labels.iloc[train_index], labels.iloc[test_index]
+
+            tree = fit(X_train, y_train)
+            y_pred = predict(tree, X_test)
+            scores.append(metrics.accuracy_score(y_test, y_pred))
+
+            tree.display()
+
+        print(
+            f"Mean (std) Accuracy (for k={kfold.n_splits} splits): {np.mean(scores)} ({np.std(scores)})"
+        )
